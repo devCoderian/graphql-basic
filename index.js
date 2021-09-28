81e7cb9d2411e1ed90d6a5abb1d2e6bd3a7016a8
devCoderian <devCoderian@gmail.com>
2021-09-28 22:44:26 +0900

    crud test

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const database = require('./database');
 console.log('프로젝트 생성');
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server');
+const { equipments } = require('./database');
 const typeDefs = gql`
     type Query{
         teams: [Team]
+        team(id: Int) : Team
+        equipments: [Equipment]
+        supplies: [Supply]
+    }
+    type Mutation {
+        insertEquipment(
+            id: String,
+            used_by: String,
+            count: Int,
+            new_or_used: String
+        ): Equipment
+        deleteEquipment(id:String): Equipment
+        editEquipment(
+            id: String,
+            used_by: String,
+            count: Int,
+            new_or_used: String
+        ):Equipment
     }
     type Team {
         id: Int
@@ -13,15 +32,65 @@ const typeDefs = gql`
         mascot: String
         cleaning_duty: String
         project: String
+        supplies: [Supply]
+    }
+    type Equipment {
+        id: String
+        used_by: String
+        count: Int
+        new_or_used: String
+    }
+    type Supply{
+        id:String
+        team: Int
     }
 `
 const resolvers = {
     Query:{
-        teams: () => database.teams
-    }
+
+        teams: () => database.teams.map((team)=> {
+            team.supplies = database.supplies.filter((supply) =>{
+                return supply.team === team.id
+            })
+            return team
+        }),
+        team: (parent, args, context, info) => 
+            database.teams.filter((team) =>{
+                return team.id === args.id
+            })[0],
+        equipments: () => database.equipments,
+        supplies: () => database.supplies,
+    },
+    Mutation: {
+        insertEquipment: (parent, args, context, info) => {
+            database.equipments.push(args)
+            return args
+        },
+        deleteEquipment: (parent, args, context, info) => {
+            const deleted = database.equipments
+                .filter((equipment) => {
+                    return equipment.id === args.id
+                })[0]
+            database.equipments = database.equipments
+                .filter((equipment) => {
+                    return equipment.id !== args.id
+                })
+            return deleted
+        },
+        editEquipment: (parent, args, context, info) => {
+          return database.equipments.filter((equipment) =>{
+              return equipment.id === args.id
+          }).map((equipment)=> {
+              Object.assign(equipment, args)
+              return equipment
+          })[0]
+        }
+  }
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers }) //서버 생성
+// typeDef => 요청의 타입 지정 gql로 생성됨
+// resolver => 서비스의 액션들을 함수로 지정/ 요청에 따라 데이터를 반환, 입력, 수정, 삭제
 server.listen().then(({url})=>{
     console.log(`Server ready at ${url}`)
 })
